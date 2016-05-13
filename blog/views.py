aef8785a066ac05736308766c2944b3341a8bb34
kyungjoongo <kyungjoon.go@gmail.com>
2016-05-13 13:36:32 +0900

    adfasfsgfdgdsfg

--- blog/views.py
+++ blog/views.py
@@ -55,12 +55,7 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
-########################################
 
-
-
-
-####################
 
 
 
